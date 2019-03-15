46278571f351afc5028f77fb183b77627cac9e3d
Jumi_hsu <scm80507211@gmail.com>
2019-03-15 18:41:11 +0800

    練習中的unity專案

---
+++ Assets/Script/TriggerCoin.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCoin : MonoBehaviour
+{
+    public GameObject soundObject;  // 稍後要拿來放產生出來的音效prefab，public type varName;
+
+    void OnTriggerEnter2D(Collider2D toucher)  // func：OnTriggerEnter2D，參數：Collider2D類別，變數名稱toucher
+    {
+        Debug.Log(toucher.name);  // 他返回的是 "進入trigger區域的那個物件" 的名稱
+
+        if(toucher.name == "mainCharacter")
+        {
+            // 產生指定的prefab，參數要嘛只用1個，要嘛3個都用
+            // Instantiate(soundObject, new Vector3(0, 0, 0),Quaternion.identity);  //生在指定位置
+            // Instantiate(soundObject, GetComponent<Transform>().position, Quaternion.identity);  //生在金幣所在處
+            var soundObj = Instantiate(soundObject, transform.position, Quaternion.identity);  //只能用在class內，外人看不到
+            Destroy(gameObject);  // gameObject=金幣
+            Destroy(soundObj);  // 音效prefab
+
+            // 雖然可以work但不太好的寫法
+            // Instantiate(soundObject, transform.position, Quaternion.identity);
+            // var target = GameObject.Find("handleCoins(Clone)");
+            // Destroy(target,1.5f);
+
+            //刪掉角色自己了!!!
+            // Destroy(toucher.gameObject);
+        }
+    }
+}
---
+++ Assets/Script/mainCharacter_localposition.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class mainCharacter_localposition : MonoBehaviour
+{
+    private Rigidbody2D m_Rigidbody2D;  // 宣告一個 私有+"Rigidbody2D"類型 的變數
+    private Animator m_Animator;  // 為了要取得 animator 的 component，所以先做一個 m_***
+
+    // Awake is called when the script instance is being loaded.
+    void Awake()
+    {
+        Debug.Log("Awake階段");
+    }
+
+    void Start()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        m_Animator = GetComponent<Animator>();
+
+        // [練習] 讓角色在 start 時，移動到特定位置 (與箱子重疊)
+        GetComponent<Transform>().localPosition = new Vector3 (2,0.6f,0); //(左右,上下,無)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 以一個速率移動：初始位置+位移量*(後一秒-前一秒)
+        // 每個影格都執行+100，那手機FPS=30，電腦FPS=120，那手機上就只會執行 100*30次，而電腦則會執行=100*120=比較多次
+        // 所以 Time.deltaTime 的值，在手機= 1/30，在電腦= 1/120
+        // 這種寫法有可能會因為在奇怪的地方產生碰撞，而發生奇怪的推擠現象
+        transform.localPosition += new Vector3(2.5f, 0, 0) * Time.deltaTime;
+        GetComponent<Transform>().localPosition += new Vector3(2.5f, 0, 0) * Time.deltaTime;
+
+    }
+
+    void LateUpdate()
+    {
+
+    }
+}
