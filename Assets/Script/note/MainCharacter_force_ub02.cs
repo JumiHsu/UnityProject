be2a5df7458a0e35f09c13e315e0fe030dc9e227
Jumi_hsu <scm80507211@gmail.com>
2019-03-26 19:40:36 +0800

    新增跳躍動作，修改跳躍的動作結構，無法用影片的方式判斷離地

--- Assets/Script/note/MainCharacter_force_ub02.cs
+++ Assets/Script/note/MainCharacter_force_ub02.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainCharacter_force : MonoBehaviour
+public class MainCharacter_force_ub02 : MonoBehaviour
 {
     // 為了要取得 Animator 的 component，所以先做一個 m_Animator
     private Rigidbody2D m_Rigidbody2D;
@@ -10,10 +10,11 @@ public class MainCharacter_force : MonoBehaviour
     private SpriteRenderer m_SpriteRenderer;
     private Transform m_transform;
 
-    // private bool isGetKeyDownIdle;
-    // private bool isGetKeyIdle;
-    // private bool isGetKeyUpIdle;
-    // private bool isPlayerIdle;
+    private bool isGround;
+    private bool isGetKeyDownIdle;
+    private bool isGetKeyIdle;
+    private bool isGetKeyUpIdle;
+    private bool isPlayerIdle;
 
     public float moveSpeed = 3.5f;
     public Vector2 moveDir;
@@ -23,13 +24,9 @@ public class MainCharacter_force : MonoBehaviour
     public float jumpSpeed = 5.0f;
     public Vector2 jumpDir;
 
-
-    public bool isGroundRaycast;  // 用raycast來判斷，但沒作用
-    public bool isGroundPosY;  // 用高度來判斷，很爛
-    
-    public bool isGrounded = false;  // 用接觸的物件的tag來判斷
-    
+    public bool isGrounded = false;
     public GameObject groundedObj;  //判斷踩著的物體是什麼
+
     public ContactPoint2D[] contacts;  // contacts是一個陣列
 
     
@@ -47,14 +44,14 @@ public class MainCharacter_force : MonoBehaviour
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
         m_transform = GetComponent<Transform>();  // 是初始位置，還是是一個會持續變更值的容器?
 
-        // isGroundRaycast = Physics.Raycast(transform.position, -Vector3.up, 0.2f);
-        // isGroundPosY = transform.position.y < 0.115f == true ;
-
-        // isGetKeyDownIdle = (Input.GetKeyDown(KeyCode.RightArrow) == false) && (Input.GetKeyDown(KeyCode.LeftArrow) == false);
-        // isGetKeyIdle = (Input.GetKey(KeyCode.RightArrow) == false)  && (Input.GetKey(KeyCode.LeftArrow)==false);
-        // isGetKeyUpIdle = (Input.GetKeyUp(KeyCode.RightArrow) == false) && (Input.GetKeyUp(KeyCode.LeftArrow) == false);
-        // isPlayerIdle = (isGetKeyDownIdle==false && isGetKeyIdle==false && isGetKeyUpIdle==false);
+        isGround = Physics.Raycast(transform.position, -Vector3.up, 0.2f);
+        // isGround = transform.position.y < 0.115f == true ;
+        isGetKeyDownIdle = (Input.GetKeyDown(KeyCode.RightArrow) == false) && (Input.GetKeyDown(KeyCode.LeftArrow) == false);
+        isGetKeyIdle = (Input.GetKey(KeyCode.RightArrow) == false)  && (Input.GetKey(KeyCode.LeftArrow)==false);
+        isGetKeyUpIdle = (Input.GetKeyUp(KeyCode.RightArrow) == false) && (Input.GetKeyUp(KeyCode.LeftArrow) == false);
+        isPlayerIdle = (isGetKeyDownIdle==false && isGetKeyIdle==false && isGetKeyUpIdle==false);
     }
+
 
 
 
@@ -63,40 +60,15 @@ public class MainCharacter_force : MonoBehaviour
     {
         // 每秒都判斷是否離地
         m_Animator.SetBool("isGrounded",isGrounded);
+        
 
-
-        // 嘗試使用另外兩個判斷
-        // isGroundPosY.SetBool("isGroundPosY",isGroundPosY);
-        // isGroundRaycast.SetBool("isGroundRaycast", isGroundRaycast);
-        if (transform.position.y < 0.115f)
-        {
-            isGroundPosY = true;
-        }
-        else
-        {
-            isGroundPosY = false;
-        }
-
-        if(Physics.Raycast(transform.position, -Vector3.up, 0.2f))
-        {
-            isGroundRaycast = true;
-        }
-        else
-        {
-            isGroundRaycast = false;
-        }
-
-
-
-        // 移動方式1：定義此物件的移動速度向量，而不是單純位移
-        // m_Rigidbody2D.velocity = new Vector2(-moveSpeed, 0);
-        // 跳躍方式1：定義此物件的移動速度向量 (不計重力)
-        // m_Rigidbody2D.velocity = new Vector2(0, jumpSpeed);
+        // 移動方式1：定義此物件的移動速度向量，而不是單純位移 (不計重力)
 
         // 按住右
         if (Input.GetKey(KeyCode.RightArrow))
         {
             moveDir.x = moveSpeed;
+
             m_SpriteRenderer.flipX = false;  // 處理轉向
             m_Animator.SetBool("isMove", true);
 
@@ -104,7 +76,8 @@ public class MainCharacter_force : MonoBehaviour
         // 按住左
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            moveDir.x = -moveSpeed;
+            moveDir.x = -moveSpeed;  // m_Rigidbody2D.velocity = new Vector2(-moveSpeed, 0);
+
             m_SpriteRenderer.flipX = true;
             m_Animator.SetBool("isMove", true);
         }
@@ -117,30 +90,63 @@ public class MainCharacter_force : MonoBehaviour
         }
 
 
+        // 跳躍方式1：定義此物件的移動速度向量 (不計重力)
+        // m_Rigidbody2D.velocity = new Vector2(0, jumpSpeed);
+
         // 跳躍方式2：對物件給予一個物理上的力(一個方向向量)，跟1的差別在，force會與向下的重力效果一起計算
         // m_Rigidbody2D.AddForce(Vector2.up * jumpForce);
 
-        // isGroundPosY = 這個條件不好，設定在空中無法按下space，雖可以阻止連跳，但會讓角色無法站在箱子上跳躍
-        // isGrounded = 這個條件沒有發動
-        // isGroundRaycast = 這個條件沒有發動
-
-        // 不做離地判斷 = 用重力的話，其實還是可在空中連跳(且越跳越高)，但會因為重力的影響，而沒那麼容易越跳越高
-        if (Input.GetKeyDown(KeyCode.Space) )
+        // && isGround = 這個條件不好，設定在空中無法按下space，雖可以阻止連跳，但會讓角色無法站在箱子上跳躍
+        // 用重力的話，雖然可在空中連跳，但會因為無法對抗重力，而可以避免越跳越高
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             m_Rigidbody2D.AddForce(Vector2.up * jumpForce);  //new Vector2 (0,1) * jumpForce=new Vector2(0, jumpForce)
             m_Animator.SetTrigger("jumpTrigger");
 
             // m_Animator.SetBool("isMove", true);
             m_Animator.SetBool("isGrounding", false);  // 這一步沒有作用
-            m_Animator.SetBool("isGrounded", true);  // 這一步沒有作用
         }
         
 
         // 去觀察施予力而跳躍的時候，當前的移動速度
         if (m_Rigidbody2D.velocity.y != 0)
         {
-            // Debug.Log("按下空白的位移速度：" + m_Rigidbody2D.velocity);
+            Debug.Log("按下空白的位移速度：" + m_Rigidbody2D.velocity);
         }
+
+
+        // // 放開空白就恢復idle很怪，應該要加上"任何時候，當 觸地+無左右按鍵輸入"，則關閉move動畫
+        // if (isGround && isPlayerIdle)  //且沒有任何壓下左右鍵
+        // {
+        //     m_Animator.SetBool("isMove", false);
+        // }
+
+        // if (isGround)
+        // {
+        //     m_Animator.SetBool("isGrounding", true);
+        // }
+
+
+
+        // // 向後(左)跳，向前(右)跳 = 按住+按住
+        // if (Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.RightArrow))
+        // {
+        //     // m_Rigidbody2D.velocity = new Vector2(moveSpeed, jumpSpeed * 0.5f);
+
+        //     moveDir.x = moveSpeed;
+        //     m_Rigidbody2D.AddForce(new Vector2(0, 5.0f));  // 5.0f 是 line101 觀察得來的，jumpForce的話會噴去外太空
+        // }
+
+        // if (Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.LeftArrow))
+        // {
+        //     // m_Rigidbody2D.velocity = new Vector2(-moveSpeed, jumpSpeed * 0.5f);
+
+        //     moveDir.x = -moveSpeed;
+        //     m_Rigidbody2D.AddForce(new Vector2(0, 5.0f));
+        //     // m_Rigidbody2D.AddForce( new Vector2 (0,jumpForce));
+        // }
+
+
 
         // 目的：讓當前的 Rigidbody2D 重力(x,y)，x等於你方向鍵的力，y=他原先的重力
         // 所以你先令一個 moveDir = (x,y)，x = moveSpeed
@@ -153,26 +159,22 @@ public class MainCharacter_force : MonoBehaviour
     }
 
 
+
     void OnCollisionStay(Collision2D other)
     {
         if (other.gameObject.CompareTag("Ground"))  // 判斷踩著的物體是不是Ground，是的話：就判斷一下他的normal值的y是多少
         {
             foreach (ContactPoint2D element in other.contacts)
             {
-                Debug.Log("element.normal.y 的值 =" + element.normal.y);
                 if (element.normal.y > 0.25f)
                 {
                     isGrounded = true;
-                    groundedObj = other.gameObject;  //如果踩著的物體，他的tag是 ground，就把他指派給 groundedObj
+                    groundedObj = other.gameObject;  //如果踩著的物體tag是 ground，就把他指派給 groundObj
                     break;
                 }
             }
         }
     }
-    // Script error: OnCollisionStay
-    // This message parameter has to be of type: Collision
-    // The message will be ignored.
-
 
     void OnCollisionExit2D(Collision2D other)
     {
@@ -180,10 +182,8 @@ public class MainCharacter_force : MonoBehaviour
         {
             groundedObj = null;
             isGrounded = false;
-            Debug.Log("other.gameObject == groundedObj成立!!");
         }
     }
-
 
 
 }
