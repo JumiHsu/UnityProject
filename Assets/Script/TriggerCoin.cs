d87d93a13b9b0948d344749aaa6bc442060f4aee
Jumi_hsu <scm80507211@gmail.com>
2019-03-29 18:41:53 +0800

    gameManager腳本+UI製作+csproj檔+library檔

--- Assets/Script/TriggerCoin.cs
+++ Assets/Script/TriggerCoin.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class TriggerCoin : MonoBehaviour
 {
     public GameObject soundObject;  // 這是一個虛擬物件。稍後要拿來放產生出來的音效prefab，public type varName;
+    
+    // public GameManager _GameManager;  // 沒有 static 的話
 
-        void OnTriggerEnter2D(Collider2D toucher)  // 參數：一個Collider2D類別 的變數， 變數名稱=toucher
+
+    void OnTriggerEnter2D(Collider2D toucher)  // 參數：一個Collider2D類別 的變數， 變數名稱=toucher
         {
             Debug.Log(toucher.name);  // 他會打印出 "進入trigger區域的那個物件" 的名稱(沒有返回東西)
 
             if (toucher.name == "mainCharacter")
             {
-                // 生在金幣所在處：transform.position = GetComponent<Transform>().position
+                // 將音效生在金幣所在處：transform.position = GetComponent<Transform>().position
                 var soundObj = Instantiate(soundObject, transform.position, Quaternion.identity);  //只能用在class內，外人看不到
+                
+                // _GameManager.gameScore += 1;  // 沒有 static 的話
+                GameManager.gameScore += 1;  // 可以直接改用 類別名稱.變數 去呼叫並指派
+                Debug.Log("分數 = " + GameManager.gameScore);
+
                 Destroy(gameObject);  // gameObject=金幣
                 Destroy(soundObj, 0.5f);  // 音效prefab，注意要等音效播完才銷毀
 
